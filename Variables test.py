812d586e607f67cf1e93d987f6894b41c8eb0cee
Jaspreet <jaspreetsinghg38@gmail.com>
2025-02-04 02:36:57 +0530

    new file

---
+++ Variables test.py
@@ -0,0 +1,19 @@
+name = "Jaspreet"
+age = 26
+price = 500.10
+
+print (name)
+print (age)
+print (price)
+print ("My name is : " ,name)
+print ("My age is : " , age)
+print(type(name))
+print(type(age))
+print(type(price))
+
+name = "jassi"
+old = False
+a = None
+
+print(type(old))
+print(type(a))
